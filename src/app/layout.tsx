b08b71edb760bb5cc9d5f0772d618c12e6d80464
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-01-05 23:55:56 +0000

    Added map fireworks and basic styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./fireworks.scss";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "@/components/Navbar";
 
@@ -19,6 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className="pyro">
+          <div className="before"></div>
+          <div className="after"></div>
+        </div>
         <NavBar />
         {children}
       </body>
